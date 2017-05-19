0c965f7fe910a81d9170b70748e2fb93624e5922
StraetenMax <maxstraeten@yahoo.com>
2017-05-19 13:52:42 +0200

    mutateur et accesseur

--- src/examCnam/Brique.java
+++ src/examCnam/Brique.java
@@ -9,11 +9,31 @@ public class Brique extends ElementFixe {
 	public Brique(){
 		super();
 		niveau =0;
-		aAfficher = true;
+		this.getAAfficher();
 	}
-	public Brique(String nom, int hauteur, int largeur, int x, int y,int niveau, boolean aAfficher){
+	public Brique(String nom, int hauteur, int largeur, int x, int y,int niveau){
 		super(nom, hauteur, largeur, x, y);
 		this.niveau = niveau;
-		this.aAfficher = aAfficher;
+		this.getAAfficher();
 	}
+	
+	//accesseurs et mutateurs
+	public int getNiveau(){
+		return niveau;
+	}
+	public void setNiveau(int niveau){
+		this.niveau = niveau;
+	}
+	public boolean getAAfficher(){
+		return aAfficher;
+	}
+	public void setAAfficher(boolean aAfficher){
+		if(this.getNiveau() == 0){
+			aAfficher = false;
+		}else{
+			aAfficher = true;
+		}
+	}
+			//méthodes
+	
 }
